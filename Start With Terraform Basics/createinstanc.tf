240d1853db9bb2c28dcb7861a3f2f0b9bb9edbd6
Omid Shariati <OmidS20m@gmail.com>
2021-04-16 23:08:08 +0430

    added createinstance.tf

---
+++ Start With Terraform Basics/createinstanc.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+    access_key = "AKIAZUDEURZVJMRXEHFQ",
+    secret_key = "SECRET_KEY",
+    region = "us-east-2"
+}
+
+resource "aws_instance" "MyFirstInstance" {
+    ami = "ami-0b6b93913be33d8f6",
+    instance_type = "t2.micro"
+}
