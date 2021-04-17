9e2bc96ac39c259ef8536cae3c8e0cddf775f387
Automated Release <AutoRelease@asax.local>
2021-04-17 14:40:40 +0430

    Succeeded windows vm

--- Start With vSphere/createinstance.tf
+++ Start With vSphere/createinstance.tf
@@ -1,7 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/vsphere/latest/docs
 provider "vsphere" {
   user           = "administrator@vsphere.local"
-  password       = "---"
+  password       = ""
   vsphere_server = "172.16.18.25"
 
   # If you have a self-signed cert
@@ -27,34 +27,34 @@ data "vsphere_network" "network" {
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
-data "vsphere_virtual_machine" "template" {
-  name          = "BuildServer"
-  datacenter_id = data.vsphere_datacenter.dc.id
-}
-
-#data "vsphere_virtual_machine" "template" {
-#  name          = "Win2K19-Basic-Template"
-#  datacenter_id = data.vsphere_datacenter.dc.id
-#}
 
 #data "vsphere_host" "hs" {
 #  name = "host1"
 #  datacenter_id = data.vsphere_datacenter.dc.id
 #}
 
-data "vsphere_ovf_vm_template" "ovf" {
-  name             = "testOVF"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
+#data "vsphere_ovf_vm_template" "ovf" {
+#  name             = "testOVF"
+#  resource_pool_id = data.vsphere_resource_pool.pool.id
+#  datastore_id     = data.vsphere_datastore.datastore.id
 
-  vsphere_ovf_vm_template = "BuildServer"
+#  vsphere_ovf_vm_template = "BuildServer"
 
-  host_system_id   = data.vsphere_host.hs.id
+#  host_system_id   = data.vsphere_host.hs.id
   #remote_ovf_url   = "https://download3.vmware.com/software/vmw-tools/nested-esxi/Nested_ESXi7.0_Appliance_Template_v1.ova"
 
   #ovf_network_map = {
   #  "Network 1": data.vsphere_network.net.id
   #}
+#}
+
+data "vsphere_virtual_machine" "template" {
+  name = "BS2"
+  datacenter_id = data.vsphere_datacenter.dc.id
+
+
+  #resource_pool_id = data.vsphere_resource_pool.pool.id
+  #datastore_id     = data.vsphere_datastore.datastore.id
 }
 
 resource "vsphere_virtual_machine" "vm" {
@@ -63,8 +63,12 @@ resource "vsphere_virtual_machine" "vm" {
   datastore_id     = data.vsphere_datastore.datastore.id
 
   num_cpus = 2
-  memory   = 1024
-  guest_id = "other3xLinux64Guest"
+  memory   = 4098
+  guest_id = "windows9Server64Guest"
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
 
   network_interface {
     network_id = data.vsphere_network.network.id
@@ -72,6 +76,6 @@ resource "vsphere_virtual_machine" "vm" {
 
   disk {
     label = "disk0"
-    size  = 20
+    size  = 90
   }
 }
