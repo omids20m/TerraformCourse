a5fc2bc7972b9ba9c5657335e3f5a1d173fdb7a2
Automated Release <AutoRelease@asax.local>
2021-04-17 13:39:23 +0430

    updated

--- Start With vSphere/createinstance.tf
+++ Start With vSphere/createinstance.tf
@@ -1,7 +1,7 @@
 #https://registry.terraform.io/providers/hashicorp/vsphere/latest/docs
 provider "vsphere" {
   user           = "administrator@vsphere.local"
-  password       = "var.vsphere_password"
+  password       = "---"
   vsphere_server = "172.16.18.25"
 
   # If you have a self-signed cert
@@ -9,23 +9,48 @@ provider "vsphere" {
 }
 
 data "vsphere_datacenter" "dc" {
-  name = "DC01"
+  name = "DevOps-DC"
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "HDD-DS02"
+  name          = "DS02-SSD-HV01"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "cluster1/Resources"
+  name          = "TFPool"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
-  name          = "public"
+  name          = "VM Network"
   datacenter_id = data.vsphere_datacenter.dc.id
 }
+
+#data "vsphere_virtual_machine" "template" {
+#  name          = "Win2K19-Basic-Template"
+#  datacenter_id = data.vsphere_datacenter.dc.id
+#}
+
+#data "vsphere_host" "hs" {
+#  name = "host1"
+#  datacenter_id = data.vsphere_datacenter.dc.id
+#}
+
+#data "vsphere_ovf_vm_template" "ovf" {
+#  name             = "testOVF"
+#  resource_pool_id = data.vsphere_resource_pool.pool.id
+#  datastore_id     = data.vsphere_datastore.datastore.id
+
+#  vsphere_ovf_vm_template = "BuildServer"
+
+#  host_system_id   = data.vsphere_host.hs.id
+  #remote_ovf_url   = "https://download3.vmware.com/software/vmw-tools/nested-esxi/Nested_ESXi7.0_Appliance_Template_v1.ova"
+
+  #ovf_network_map = {
+  #  "Network 1": data.vsphere_network.net.id
+  #}
+#}
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "terraform-test"
